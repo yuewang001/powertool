c7cddcbf771adb375ec6f92215bc36343df7b802
yuewang001 <wangyue.china@live.cn>
2018-08-07 15:02:58 +0800

    fist submit
    Change-Id: If5e73e113bdf0c90fb0fe1a9635d42afd3ffac37
    Signed-off-by: yuewang001 <wangyue.china@live.cn>

---
+++ adpss_analysis/comparevalue.py
@@ -0,0 +1,143 @@
+import os
+import commands
+import time
+
+file1='C:\\Users\\yuewang\\Desktop\\version2\\Ser_result.txt'
+file2='C:\\Users\\yuewang\Desktop\\version2\\Par_result.txt'
+resultfile='C:\\Users\\yuewang\\Desktop\\version2\\result5.txt'
+
+
+time_slice_on=1
+time_slice_begin=49.100000
+time_slice_end=50.200000
+
+bool_time=0
+time_slice=[]
+
+
+
+muxian=1 #1--bus  0--GEN
+x1=[]
+x2=[]
+
+def writefile(filename, data):
+    fo = open(filename, "w")
+    for line in data:
+        for astr in line:
+            #print astr
+            #print xx
+            
+            #xx=("%.6f"  % float(astr))
+            tmp=str(astr)+"   "
+            fo.write(tmp)
+        
+        fo.write("\n")
+            
+    fo.close
+    
+    
+try:
+        with open(file1,'r') as f1:
+            for line in f1:
+                tmp=[]
+                
+                #judge if time lice is within defined .
+                #print line
+                #print line.split()[0]
+
+
+                if(time_slice_on == 1):
+                    time_value=float(line.split()[0])
+                    print time_value
+                    if(time_value<time_slice_begin):
+                        for x in line.split():
+                            tmp.append(x)
+                        x1.append(tmp) 
+                        continue
+                    if(time_value>time_slice_end):
+                        break
+                else:
+                    for x in line.split():
+                        tmp.append(x)
+                    x1.append(tmp) 
+               
+        
+        with open(file2,'r') as f2:
+            for line in f2:
+                
+                tmp=[]
+                
+                #judge if time lice is within defined .
+                
+                
+                #print time_value
+                if(time_slice_on == 1):
+                    time_value=float(line.split()[0])
+                    if(time_value<time_slice_begin):
+                        for x in line.split():
+                            tmp.append(x)
+                        x2.append(tmp)
+                        continue
+                    if(time_value>time_slice_end):
+                        break
+                else:
+                    #print " file 2 open, no time slice on\n"
+                    for x in line.split():
+                        tmp.append(x)
+                    x2.append(tmp) 
+          
+                 
+                
+except:
+        print "open file error "
+        
+print "len of x1:"+str(len(x1))+"\n"
+print "len of x2:"+str(len(x2))+"\n"
+        
+lg=len(x1)
+data=[]
+maxdata=[]
+for i in range(len(x1)):
+    #print i
+    tmp=[]
+    tmp.append(x1[i][0])
+    for j in range(1,len(x1[i])):
+        #print x1[i][j]
+        #print x2[i][j]
+        t=abs(float(x1[i][j])-float(x2[i][j]))
+        if(muxian==1):
+            if(j  % 2) == 1:
+                t=abs(100*t/float(x1[i][j]))
+        tmp.append(round(t,6))
+      
+    data.append(tmp)
+
+row_num=len(data)
+col_num=len(data[0])
+ 
+print row_num
+print "-----"
+print col_num
+print "-----"
+
+cc=0
+
+if(muxian==0 or muxian==1):
+    max_row=[]
+    max_row.append("-----")
+    for i in range(1,col_num):
+        #print "i="+str(i)+"\n"
+        #max_row=[]
+        max_tmp=0
+        for j in range(row_num):
+            xx=float(data[j][i])
+            if(xx>max_tmp):
+                max_tmp=xx
+        max_row.append(round(max_tmp,6))
+
+    data.append(max_row)
+    print "len of max value:"+str(len(max_row))
+            
+               
+writefile(resultfile,data)
+print "----------Done  ----\n"
---
+++ adpss_analysis/extractfile.py
@@ -0,0 +1,212 @@
+import os
+import commands
+import time
+
+
+debug=0
+Linux=0
+if(Linux==0):   #windows 
+	keyfile='D:\\Data\\201806XinanHuadongSerParComp\\XiNanBUS\\Name.txt'    #key file 
+	valueFile='D:\\Data\\201806XinanHuadongSerParComp\\XiNanBUS\\SimpleVariablesPar.txt'    #SimpleVariables.txt file
+	#valueFile1='......\\SimpleVariablesPar.txt'  
+	#valueFile2='......\\SimpleVariablesSer.txt'  
+	prefix='D:\\Data\\201806XinanHuadongSerParComp\\XiNanBUS\\WholeSubNet\\'     #simulation result file
+	result_file='D:\\Data\\201806XinanHuadongSerParComp\\XiNanBUS\\Par_result.txt'    
+else:
+	keyfile='/jenkins/mywork/Name.txt'
+	valueFile='/jenkins/mywork/file2.txt'
+	prefix='/jenkins/result/res/'
+	result_file='/jenkins/seq_result2.txt'
+	
+time_slice_on=1
+time_slice_begin=50.000000
+time_slice_end=60.000000
+
+bool_time=0
+time_slice=[]
+
+
+def readkeylist(keyfile):
+	keyList=[]
+	with open(keyfile,'r') as f:
+		for key in f:
+			#print key
+			keyList.append(key.strip())
+			#break
+	f.close()
+	return keyList
+
+def getfilename(key,locationFile):
+	find=0
+	infoList=[]
+	with open(locationFile,'r') as f1:
+		for line in f1:
+			#print line
+			tmp=line.split(',')
+			#print tmp[3]
+			if(len(tmp)<4):
+				continue
+			tmp1=tmp[3].strip()
+			#print tmp1
+			if(key==tmp1):
+				#print "same"
+				#break
+				dir=tmp[6].strip()
+				filename=tmp[7].strip()
+				col=tmp[8].strip()
+				find=find+1
+				infoList.append([dir,filename,col])
+				if(find==2):
+					break
+			#keyList.append(key)
+	f1.close()
+	return infoList	
+#aindex is 1 parallel ,0 sequenial 
+def getRealValue(afolder,afilename,colNum):
+	
+	keyList=[]
+	
+	if(Linux==1):
+		fullpathFileName=prefix+afolder+'//'+afilename
+	else:
+		fullpathFileName=prefix+afolder+'\\'+afilename
+	#count=0
+	try:
+		with open(fullpathFileName,'r') as f:
+			print "getRealValue successfully on read file :"+fullpathFileName+"---\n"
+			#print f
+			for line in f:
+				#print key
+				tmp=line.split(',')
+				#print tmp.strip()
+				col=int(colNum)
+				
+				time_value=float(tmp[0].strip())
+				if(time_slice_on == 1):
+					if(time_value<time_slice_begin):
+						continue
+					if(time_value>time_slice_end):
+						break
+				#print col
+				#xx=key.strip()
+
+				keyList.append(tmp[col].strip())
+				
+#				 if(debug==1):
+#					 count=count+1
+#					 if(count>20):
+#						 break
+		print "getRealValue successfully  :"+fullpathFileName+"---\n"
+		return keyList
+	except:
+		print "getRealValue error on read file :"+fullpathFileName+"---col:"+colNum+"\n"
+		return 
+	
+	
+def getTimeSlice(afolder, afilename):
+
+	keyList=[]
+	if(Linux==1):
+		fullpathFileName=prefix+afolder+'//'+afilename
+	else:
+		fullpathFileName=prefix+afolder+'\\'+afilename
+	#count=0
+	try:
+		with open(fullpathFileName,'r') as f:
+			for line in f:
+				tmp=line.split(',')
+				time_value=float(tmp[0].strip())
+				if(time_slice_on == 1):
+					if(time_value<time_slice_begin):
+						continue
+					if(time_value>time_slice_end):
+						break
+				keyList.append(tmp[0].strip())
+#				 if(debug==1):
+#					 count=count+1
+#					 if(count>20):
+#						 break
+		return keyList
+	except:		   
+		print "getTimeSlice get error---------on  open file:"+afolder+"---"+afilename
+		return 
+
+def writefile(filename, data):
+	fo = open(filename, "w")
+	for line in data:
+		for astr in line:
+			tmp=str(astr)+"   "
+			fo.write(tmp)
+		fo.write("\n")
+			
+	fo.close
+
+   
+if __name__ == "__main__":
+		
+		x=readkeylist(keyfile)
+		key_location=[]
+		val_result=[]
+		
+		time_val_seq=0
+		time_val_para=0
+		#read time line
+		step=0;
+		key_len=len(x)
+		for key in x:
+			index_array=getfilename(key,valueFile)
+			key_location.append([key,index_array])
+			print key_location
+			
+			################################################
+		
+		count=0
+		for one in key_location:
+			count=count+1
+			print "xxxxxxxxxxxxxx\n"
+			print "num key: "+str(count)
+			print one
+			print "xxxxxxxxxxxxxx\n"
+			
+			value=getRealValue(one[1][0][0],one[1][0][1],one[1][0][2])
+			if type(value)==list:
+				if(time_val_seq<len(value)):
+					time_val_seq=len(value)
+				
+				val_result.append(value)
+			value=getRealValue(one[1][1][0],one[1][1][1],one[1][1][2])
+			val_result.append(value)
+	 
+	
+		
+			if(bool_time==0):
+				time_slice=getTimeSlice(index_array[0][0],index_array[0][1])
+				bool_time=1
+		
+		data_print=[]
+		#print val_result
+		print "time step: "+str(len(time_slice))
+		for i in range(len(time_slice)):
+			#print time_slice[i]  
+			#print "i="+str(i)
+			tmp_matrix=[]
+# 			if(time_slice_on == 1):
+# 				if(float(time_slice[i])<time_slice_begin):
+# 					continue
+# 			
+# 				if(float(time_slice[i])>time_slice_end):
+# 					break
+			tmp_matrix.append(time_slice[i])
+			for j in range(len(val_result)):
+				#print "j="+str(j)
+				tmp_matrix.append(val_result[j][i])
+			data_print.append(tmp_matrix)
+		writefile(result_file,data_print)
+		
+		print "Done-------------------"
+		#print val_seq
+		print "col num:"+str(len(val_result))
+	
+		print "Done-------------------"
+		
+		
